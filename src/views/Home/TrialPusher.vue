2535b7da6860da2914ab978c0bb2524e2e30d340
Mzala123 <justicemwanzamj@gmail.com>
2022-08-28 20:46:40 +0200

    sample chat

--- src/views/Home/TrialPusher.vue
+++ src/views/Home/TrialPusher.vue
@@ -3,39 +3,62 @@
     <v-card>
       <div></div>
       <v-form>
-        <v-btn
-          color="primary"
-          class="mr-5 ml-5 pt-1 mb-4"
-          elevation="1"
-          @click="vote"
-        >
-          Vote
-        </v-btn>
+        <div v-if="!joined">
+          <v-text-field
+            class="mr-5 ml-5 pt-4 mb-2"
+            outlined
+            dense
+            v-model="current_user"
+            label="Join"
+          >
+          </v-text-field>
+          <v-btn
+            color="primary"
+            class="mr-5 ml-5 pt-1 mb-4"
+            elevation="1"
+            @click="vote"
+          >
+            Join
+          </v-btn>
+        </div>
+        <div v-if="joined">
+          <div>
+            <v-textarea  class="mr-5 ml-5 pt-4 mb-2"
+             v-on:keyup.enter="sendMessage"
+             v-model="text"
+             filled name="input-7-4" label="Filled textarea">
+            </v-textarea>
+          </div>
+        </div>
       </v-form>
     </v-card>
   </v-container>
 </template>
 
-<script> 
+<script>
 //import io from 'socket.io-client'
 export default {
   data() {
     return {
       socket: null,
-      joined: false
-    }
+      joined: false,
+      current_user: null,
+      text:""
+    };
   },
-  
+
   methods: {
     vote() {
-      this.joined = true
-     // this.socket = io('http://localhost:3000')
+      console.log(this.current_user);
+      this.joined = true;
+      // this.socket = io('http://localhost:3000')
     },
-    
+    sendMessage(){
+      console.log(this.text)
+      //this.$socket
+    }
   },
 
-  created(){
-
-  }
+  created() {},
 };
 </script>
